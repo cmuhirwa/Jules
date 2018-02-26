575d34962a91ed7015c9349ffb8cf284e9c324f5
gokigaligo <clementmuhirwa@gmail.com>
2018-02-26 14:16:57 +0200

    gfggf

---
+++ start.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<!-- This site was created in Intopsite. http://www.Intopsite.com -->
+<!-- Last Published: Sat Dec 02 2017 20:04:13 GMT+0000 (UTC) -->
+<html data-wf-domain="mukundwas-amazing-project.Intopsite.io" data-wf-page="5990083b7658f50001a2dcbd" data-wf-site="5990083b7658f50001a2dcbc" data-wf-status="1">
+    <head>
+	    <meta charset="utf-8"/>
+        <title>Mukundwa&#x27;s Amazing Project</title>
+        <meta content="width=device-width, initial-scale=1" name="viewport"/>
+        <meta content="Intopsite" name="generator"/>
+        <link href="css/start.style.css" rel="stylesheet" type="text/css"/>
+        <script src="js/start.webfont.js" type="text/javascript">
+        </script>
+        <script type="text/javascript">WebFont.load({  google: {    families: ["Exo:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic"]  }});</script>
+        <!--[if lt IE 9]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript">
+        </script>
+        <![endif]-->
+        <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+        <link href="#" rel="shortcut icon" type="image/x-icon"/>
+        <link href="#" rel="apple-touch-icon"/>
+    </head>
+    <body>
+
+
+	    <div class="content">
+
+
+	        <div class="home">
+	            <img src="images/59ee1adf3f3dda00010e44f0_1%20(5).jpg" class="image-13"/>
+                <div class="div-block-6">
+	                <div class="div-block-7">
+	                    <div class="text-block">Find your perfect domain name</div>
+                        <div class="form-block w-form">
+	                        <form id="email-form" name="email-form" data-name="Email Form" class="w-clearfix">
+	                            <input type="text" class="text-field w-input" maxlength="256" name="name-2" data-name="Name 2" placeholder="Find your perfect domain name" id="name-2"/>
+                                <input type="submit" value="Search" data-wait="Please wait..." class="submit-button w-button"/>
+                            </form>
+                        </div>
+                        <h1 class="heading-7">exmaple.com is taken</h1>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <h1 class="heading-10">thank you visit this page but is unavailable on your device</h1>
+            <div class="div-block-8">
+	            <div class="div-block-9">
+	                <h1 class="heading-2">Get website now</h1>
+                    <h1 class="heading-3">From small to big business to enterprise, we&#x27;ve got you covered!</h1>
+                    <a href="#" class="link-4">Read  before choose this step</a>
+                </div>
+                <div class="host">
+	                <div class="form-block-3 w-form">
+	                    <form id="email-form-3" name="email-form-3" data-name="Email Form 3" class="form">
+	                        <div class="div-block-10 w-clearfix">
+
+	                            <div class="div-block-12">
+	                                <h1 class="heading-4">L. business</h1>
+                                    <div class="div-block-13">
+                                        <h1 class="heading-5">Support 7/24</h1>
+                                        <h1 class="heading-5">Build website</h1>
+                                        <h1 class="heading-5">1 year guarantee</h1>
+                                        <h1 class="heading-5">website run 1 year</h1>
+                                    </div>
+                                    <div class="radio-button-field w-radio">
+	                                    <input type="radio" id="radio" name="radio" value="Radio" data-name="Radio" class="radio-button w-radio-input"/>
+                                        <label for="radio" class="field-label-2 w-form-label">Select</label>
+                                    </div>
+                                 </div>
+
+                                <div class="div-block-12 h">
+	                                <h1 class="heading-4">H. business</h1>
+                                    <div class="div-block-13 h">
+                                        <h1 class="heading-5">System</h1>
+                                        <h1 class="heading-5">Interface</h1>
+                                        <h1 class="heading-5">Advertising</h1>
+                                        <h1 class="heading-5">Support 7/24</h1>
+                                        <h1 class="heading-5">1 year guarantee</h1>
+                                        <h1 class="heading-5">website run 1 year</h1>
+                                    </div>
+                                    <div class="radio-button-field w-radio">
+                                	    <input type="radio" id="radio-2" name="radio" value="Radio" data-name="Radio 2" class="radio-button w-radio-input"/>
+                                        <label for="radio-2" class="field-label-2 w-form-label">Select</label>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="div-block-12 n">
+	                                <h1 class="heading-4">N. business</h1>
+                                    <div class="div-block-13 n">
+                                        <h1 class="heading-5">Advertising</h1>
+                                        <h1 class="heading-5">Build website</h1>
+                                        <h1 class="heading-5">Support 7/24</h1>
+                                        <h1 class="heading-5">2 year warranty</h1>
+                                        <h1 class="heading-5">website run 1 year</h1>
+                                    </div>
+                                    <div class="radio-button-field w-radio">
+                                        <input type="radio" id="radio-3" name="radio" value="Radio" data-name="Radio 3" class="radio-button w-radio-input"/>
+                                        <label for="radio-3" class="field-label-2 w-form-label">Select</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="purpose">
+	            <h1 class="heading-8">Choose your website purpose</h1>
+                <div class="form-block-4 w-form">
+	                <form id="email-form-4" name="email-form-4" data-name="Email Form 4" class="form-2">
+                        <select id="field-2" name="field-2" class="select-field w-select">
+                            <option value="">Select one...</option>
+                            <option value="First">Tv or Radio</option>
+                            <option value="Second">Sell or work  online</option>
+                            <option value="Third">Business, School, hospital </option>
+                            <option value="Another Choice">Others</option>
+                        </select>
+                    </form>
+                </div>
+            </div>
+
+
+            <div class="purpose ssc">
+	            <h1 class="heading-8">SSL Certificates</h1>
+                <div class="text-block-4 csss">Give your website high security and https and high security <br/>Secure credit card numbers, passwords and other sensitive data your visitors submit through your site</div>
+                <div class="form-block-4 css w-form">
+	                <form id="email-form-4" name="email-form-4" data-name="Email Form 4" class="form-2 h">
+	                    <div class="radio-button-field-2 w-radio">
+	                        <input type="radio" id="radio-4" name="radio-4" value="Radio 4" data-name="Radio 4" class="radio-button-2 w-radio-input"/>
+                            <label for="radio-4" class="field-label-3 w-form-label">Add standard SSL Certificate</label>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
+
+            <div class="div-block-14">
+	            <h1 class="heading-6">Tell us others thing you want on your website</h1>
+                <div class="text-block-2">Comments, wishes or others service you need on your website</div>
+                <div class="form-block-5 w-form">
+	                <form id="email-form-5" name="email-form-5" data-name="Email Form 5" class="w-clearfix">
+	                    <textarea id="field-3" name="field-3" placeholder="Comments" maxlength="5000" class="textarea-2 w-input"></textarea>
+                        <input type="submit" value="Send information" data-wait="Please wait..." class="submit-button-3 w-button"/>
+                    </form>
+                </div>
+            </div>
+
+
+            <div class="footer">
+	            <h1 class="heading-9">Copyright 2017 | Intopsite Ltd | All Rights Reserved</h1>
+            </div>
+
+
+        </div>
+
+
+        <div class="head">
+	        <div class="div-block-2 w-clearfix">
+	            <div class="div-block-3 e">
+	                <h1 class="heading">Email : customercare@instosite.com</h1>
+                    </div>
+                    <div class="div-block-3 w w-clearfix">
+	                    <div class="div-block-4">
+	                       <img src="images/59900c5222e8860001b03e12_598de93390707d00013b6f58_597b5f7ab81a770001f1cdce_facebook.png" class="image"/>
+                            <img src="images/59900c5222e8860001b03e13_598de93390707d00013b6f59_597b603e23a85d000177fbe2_seeicons__57ac2d3203ca4-p-500.png" class="image"/><img src="images/59900c2e90707d00013c4b08_598de936c327c10001ed22b4_597b608a13db9e000118da4c_8800.png" class="image"/><img src="images/59900c2e90707d00013c4b09_598de937153f32000198d406_597b632ba772fc0001fd68e2_34227.png" class="image w"/>
+                    </div>
+                    <div class="div-block-4 w w-clearfix">
+                        <a href="/login-registration" class="button w-button">Login</a>
+                        <a href="/login-registration" class="button w-button">Register</a>
+                        <a href="#" class="link-3">My Account</a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div data-collapse="tiny" data-animation="default" data-duration="400" class="navbar w-nav">
+	            <div class="container w-container">
+	                <a href="#" class="brand w-nav-brand">
+	                    <img src="images/599010b8c327c10001ee0fec_eefcfa51-8a06-42b5-a1bc-aaedf3505918%20-%20Copy.png" class="image-12"/>
+                    </a>
+                    <nav role="navigation" class="nav-menu w-nav-menu">
+                        <a href="#" class="nav-link ho w-nav-link">Home</a>
+                        <a href="#" class="nav-link ho w-nav-link"> Cart</a>
+                        <a href="#" class="nav-link ho w-nav-link">Contact</a>
+                        <a href="#" class="nav-link ho w-nav-link">Logout</a>
+                    </nav>
+                    <div class="menu-button w-nav-button">
+	                   <div class="icon w-icon-nav-menu"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+<script src="js/jquery.min.js" type="text/javascript">
+</script>
+<script src="js/start.jss" type="text/javascript">
+</script>
+<!--[if lte IE 9]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js">
+</script>
+<![endif]-->
+</body>
+</html>
